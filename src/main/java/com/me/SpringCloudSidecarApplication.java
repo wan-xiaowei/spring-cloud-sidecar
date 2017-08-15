c30170601efa93aa1e788606cd60b59225f8f166
“w“wanxiaowei <260483900@qq.com>
2017-08-16 01:20:24 +0800

    update

--- src/main/java/com/me/SpringCloudSidecarApplication.java
+++ src/main/java/com/me/SpringCloudSidecarApplication.java
@@ -2,8 +2,10 @@ package com.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
+@EnableSidecar  //整合了EnableCircuitBreaker  EnableDiscoveryClient  EnableZuulProxy
 public class SpringCloudSidecarApplication {
 
 	public static void main(String[] args) {
