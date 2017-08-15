1ae7609bff3d6948d281fb676ecfb477a534d230
“w“wanxiaowei <260483900@qq.com>
2017-08-16 00:53:15 +0800

    init

---
+++ src/main/java/com/me/SpringCloudSidecarApplication.java
@@ -0,0 +1,12 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSidecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSidecarApplication.class, args);
+	}
+}
